f515995dc51f2a9b145c3e0ac7638bd5a566ea62
ogliore <43612703+ogliore@users.noreply.github.com>
2018-10-17 16:12:52 -0500

    Add files via upload

--- ShellScripts/TescanRenameFilesMontage.sh
+++ ShellScripts/TescanRenameFilesMontage.sh
@@ -5,27 +5,28 @@
 # $3 = output tile size in mosaic (in pixels, e.g. 100)
 # $4 = extension (e.g. jpg)
 # $5 = output file (e.g. ~/Documents/panorama.jpg)
+# montage memory issues: https://github.com/ImageMagick/ImageMagick/issues/396
 
 STARTTIME=`date +%s`
 
 MONTAGEOVERLAP=$(echo "scale=2 ; $3*($2 / 2)/100" | bc)
 
-
 cd $1
 
-# TILETEXT=$(ls [0-9]*.jpg | tail -1 | cut -f 1 -d '.' | tr _ x)
-# TILETEXT=$(ls 001_[0-9]*.jpg | tail -1 | cut -f 1 -d '.' | tr _ x)
-NX=$(ls -l *_001.$4 | wc -l)
-#echo $TITLETEXT
+FIRSTROW=$(ls *.$4 | head -1 | awk -v FS="(_|\.)" '{print $2}')
+MONTAGEFORMATCODE=%0${#FIRSTROW}d
+
 find -regex '\./[0-9_]+' -type d -exec mv -n -- {}/Snap_1.$4 {}.$4 2>/dev/null \;
 find -regex '\./[0-9_]+' -type d -exec mv -n -- {}/Snap_1-$4.hdr {}-$4.hdr 2>/dev/null \; -empty -delete
-#rm -f panorama.jpg
-#montage *.jpg -limit area 0 -limit memory 0 -limit map 0 -rotate -90 -border -$2% -geometry $3x -tile $TILETEXT  panorama.jpg
+
+NX=$(ls -l *_$FIRSTROW.$4 | wc -l)
+
 mkdir -p $MAGICK_TEMPORARY_PATH/stripimages_
+
 for i in $( seq 1 $NX )
 do
 echo $(printf "%03d" $i)
-montage -rotate -90 -geometry $3x-$MONTAGEOVERLAP -tile x1 $(printf "%03d" $i)_*.$4 $MAGICK_TEMPORARY_PATH/stripimages_/strip$(printf "%03d" $i).$4
+montage -rotate -90 -geometry $3x-$MONTAGEOVERLAP -tile x1 $(printf $MONTAGEFORMATCODE $i)_*.$4 $MAGICK_TEMPORARY_PATH/stripimages_/strip$(printf $MONTAGEFORMATCODE $i).$4
 done
 montage -geometry 1x1+0-$MONTAGEOVERLAP'<' -tile 1x $MAGICK_TEMPORARY_PATH/stripimages_/strip*.$4 $5
 rm -rf $MAGICK_TEMPORARY_PATH/stripimages_
