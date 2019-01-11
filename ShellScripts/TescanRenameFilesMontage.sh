1e73d2bcd984a599387ced600f5ebde458f65864
ogliore <43612703+ogliore@users.noreply.github.com>
2019-01-11 00:26:38 -0600

    Add files via upload

--- ShellScripts/TescanRenameFilesMontage.sh
+++ ShellScripts/TescanRenameFilesMontage.sh
@@ -1,40 +1,44 @@
 #!/bin/sh
 
 # $1 = directory
-# $2 = overlap of tiles (in percent, e.g. 10)
-# $3 = output tile size in mosaic (in pixels, e.g. 100)
-# $4 = extension (e.g. jpg)
-# $5 = output file (e.g. ~/Documents/panorama.jpg)
+# $2 = horizontal overlap of tiles (in percent, e.g. 10)
+# $3 = vertical overlap of tiles (in percent, e.g. 10)
+# $4 = output tile size in mosaic (in pixels, e.g. 100)
+# $5 = extension (e.g. jpg)
+# $6 = output file (e.g. ~/Documents/panorama.jpg)
 # montage memory issues: https://github.com/ImageMagick/ImageMagick/issues/396
+# First test this script on four images from the middle of the panorama. Must rename the images to 001_001.jpg, 001_002.jpg, 002_001.jpg, 002_002.jpg. Adjust $2 and $3 until blend is good.
+# Max supported dimension for imagemagick is 65500 pixels
+# nm/pixel for panorama will be: (original image nm/pixel) * ($4/original-image-size)
 
 STARTTIME=`date +%s`
 
-INCREASEOVERLAPFAC=1.21
-INCREASEOVERLAPFACSTRIP=1
+#INCREASEOVERLAPFAC=1.21
+#INCREASEOVERLAPFACSTRIP=1
 #0.18
 
-MONTAGEOVERLAP=$(echo "scale=2 ; $INCREASEOVERLAPFAC*$3*($2 / 2)/100" | bc)
-MONTAGEOVERLAPSTRIP=$(echo "scale=2 ; $INCREASEOVERLAPFACSTRIP*$3*($2 / 2)/100" | bc)
+MONTAGEOVERLAP=$(echo "scale=2 ; $4*($2 / 2)/100" | bc)
+MONTAGEOVERLAPSTRIP=$(echo "scale=2 ; $4*($3 / 2)/100" | bc)
 
 cd $1
 
-FIRSTROW=$(ls *.$4 | head -1 | awk -v FS="(_|\.)" '{print $2}')
+FIRSTROW=$(ls *.$5 | head -1 | awk -v FS="(_|\.)" '{print $2}')
 MONTAGEFORMATCODE=%0${#FIRSTROW}d
 
-find -regex '\./[0-9_]+' -type d -exec mv -n -- {}/Snap_1.$4 {}.$4 2>/dev/null \;
-find -regex '\./[0-9_]+' -type d -exec mv -n -- {}/Snap_1-$4.hdr {}-$4.hdr 2>/dev/null \; -empty -delete
+find -regex '\./[0-9_]+' -type d -exec mv -n -- {}/Snap_1.$5 {}.$5 2>/dev/null \;
+find -regex '\./[0-9_]+' -type d -exec mv -n -- {}/Snap_1-$5.hdr {}-$5.hdr 2>/dev/null \; -empty -delete
 
-NX=$(ls -l *_$FIRSTROW.$4 | wc -l)
+NX=$(ls -l *_$FIRSTROW.$5 | wc -l)
 
 mkdir -p $MAGICK_TEMPORARY_PATH/stripimages_
 
 for i in $( seq 1 $NX )
 do
 echo $(printf "%03d" $i)
-montage -tile x1 -geometry $3x-$MONTAGEOVERLAP  $(ls -1 *_$(printf $MONTAGEFORMATCODE $i).$4 | sort -r) $MAGICK_TEMPORARY_PATH/stripimages_/strip$(printf $MONTAGEFORMATCODE $i).$4
+montage -tile x1 -geometry $4x-$MONTAGEOVERLAP  $(ls -1 *_$(printf $MONTAGEFORMATCODE $i).$5 | sort -r) $MAGICK_TEMPORARY_PATH/stripimages_/strip$(printf $MONTAGEFORMATCODE $i).$5
 done
 
-montage -tile 1x -geometry 1x1+0-$MONTAGEOVERLAPSTRIP'<' $MAGICK_TEMPORARY_PATH/stripimages_/strip*.$4 $5
+montage -tile 1x -geometry 1x1+0-$MONTAGEOVERLAPSTRIP'<' $MAGICK_TEMPORARY_PATH/stripimages_/strip*.$5 $6
 
 rm -rf $MAGICK_TEMPORARY_PATH/stripimages_
 
